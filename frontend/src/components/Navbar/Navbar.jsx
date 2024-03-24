418b20a83b57ce903cc8005fa64e3af7f29766fc
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-03-24 22:00:50 +0530

    Nav bar file

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,6 +6,20 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <img src={assets.logo} alt=""/>
+      <ul className="navbar-menu">
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile-app</li>
+        <li>contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt=""/>
+        <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt=""/>
+            <div className='dot'></div>
+        </div>
+        <button>sign in</button>
+      </div>
     </div>
   )
 }
