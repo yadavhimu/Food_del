da836bfd1ba06d23938baa0557b39be37669a958
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-04-02 21:39:36 +0530

    menu list update

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
+
 const ExploreMenu = () => {
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore our menu</h1>
       <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes.</p>
       <div className='explore-menu-list'>
-        {menu_list.map(()=>{
-            return
+        {menu_list.map((item,index)=>{
+            return (
+                <div key={index} className='explore-menu-list-item'>
+                    <img src={item.menu_image} alt=""/>
+                    <p>{item.menu_name}</p>
+                </div>    
+            )
         })}
       </div>
     </div>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 const Home = () => {
   return (
     <div>
       <Header/>
+      <ExploreMenu/>
     </div>
   )
 }
