2f3d2eb645c326008fd50f81a3df59a6808cd3df
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-04-05 08:32:57 +0530

    menu list done

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -6,7 +6,7 @@ const ExploreMenu = () => {
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore our menu</h1>
-      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes.</p>
+      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes.Our mission is to satisfy your cravings and elevate your dining experience,one delicious meal at a time.</p>
       <div className='explore-menu-list'>
         {menu_list.map((item,index)=>{
             return (
@@ -14,9 +14,7 @@ const ExploreMenu = () => {
                     <img src={item.menu_image} alt=""/>
                     <p>{item.menu_name}</p>
                 </div>    
-            )
-            
-            
+            )    
         })}
       </div>
     </div>
