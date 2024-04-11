98ce727303724ff6263b37fc7cd5590da3375bc6
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-04-11 17:17:38 +0530

    list done

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+
 const Home = () => {
+
+    const [category, setCategory]= useState("All");
+
+    
   return (
     <div>
       <Header/>
-      <ExploreMenu/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
