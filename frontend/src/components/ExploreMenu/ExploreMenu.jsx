6cca62e02e0e3f0fc5caf33e7a4ceb6980d31de3
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-04-16 19:50:43 +0530

    menu css

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -11,12 +11,11 @@ const ExploreMenu = ({category,setCategory}) => {
         {menu_list.map((item,index)=>{
             return (
                 <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)}key={index} className='explore-menu-list-item'>
-                    <img src={item.menu_image} alt=""/>
+                    <img className={category===item.menu_name?"active":""}src={item.menu_image} alt=""/>
                     <p>{item.menu_name}</p>
                 </div>    
             )   
         })}
-        
       </div>
       <hr/>
     </div>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState} from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
