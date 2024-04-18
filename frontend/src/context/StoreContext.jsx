32e86709d36f898008812ee90f70b4893fdba0ec
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-04-18 22:28:10 +0530

    dishes

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,9 +1,24 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
+
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
+    const [cartItems,setCartItems]= useState({})
+
+   const addToCart = (itemId) => {
+    if(!cartItems[itemId]){
+        setCartItems((prev)=>({...prev,[itemId]:1}))
+    }
+    else {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+    }
+   }
+
+   const removeFromCart = (itemId) =>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+   }
     const contextValue = {
         food_list
     }
