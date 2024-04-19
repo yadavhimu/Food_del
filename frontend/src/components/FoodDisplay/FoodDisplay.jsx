5dafd85a7d80aa8a35ab7dc5b5f07e5459ce95d3
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-04-19 14:02:04 +0530

    menu filtered

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -12,7 +12,9 @@ const foodDisplay = ({category}) => {
       <h2>Top dishes near you</h2>
       <div className='food-display-list'>
         {food_list.map((item,index)=>{
-            return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+            if(category==='All' || category===item.category){
+                return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+            } 
         })}
       </div>
     </div >
