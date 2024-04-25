a90d95654e8a9a79b65508fe529932e7095763a4
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-04-25 17:55:34 +0530

    place order

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -23,12 +23,47 @@ const Cart = ()=> {
           if(cartItems[item._id]>0)
           {
             return (
+              <>
               <div className='cart-items-title cart-items-item'>
+                <img src={item.image} alt=''/>
                 <p>{item.name}</p>
-                </div>
+                <p>${item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>${item.price*cartItems[item._id]}</p>
+                <p onClick={()=>removeFromCart(item._id)}className='cross'>X</p>
+              </div>
+                <hr/>
+                </>
             )
           }
         })}
+      </div>
+      <div className='cart-bottom'>
+        <div className='cart-total'>
+          <h2>Cart Totals</h2>
+          <div>
+            <div className='cart-total-detail'>
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr/>
+            <div className='cart-total-detail'>
+              <p>Delivery Fee</p>
+              <p>{2}</p>
+            </div>
+            <hr/>
+            <div className='cart-total-detail'>
+              <b>Total</b>
+              <b>{0}</b>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>if you have promo code, enter it here</p>
+          </div>
+        </div>
       </div>
     </div>
   )
